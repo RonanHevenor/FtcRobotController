e53214211b887d22fbe5d71b9ef0ea891fb8be62
Ronan <spacechickenrobot@gmail.com>
2022-12-20 14:36:32 -0500

    some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.autos.Base;
 import org.firstinspires.ftc.teamcode.autos.Mark_I;
+import org.firstinspires.ftc.teamcode.autos.Mark_II;
 import org.firstinspires.ftc.teamcode.subsystems.Drive;
 import org.firstinspires.ftc.teamcode.subsystems.System;
 
@@ -13,13 +14,18 @@ public class auto extends LinearOpMode {
 
     public Drive drive;
     public System system;
-    public Mark_I code;
+    public Mark_II code;
 
     @Override
     public void runOpMode() {
+
+        waitForStart();
+
         drive = new Drive(hardwareMap);
         system = new System(hardwareMap);
 
-        code = new Mark_I(drive, system);
+        code = new Mark_II(drive, system);
+
+        code.pos_II(drive, system, 2);
     }
 }
